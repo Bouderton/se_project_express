95515868181a78dae613169bed507fceaaa6ac01
bouderton <rmkhazal@gmail.com>
2024-04-05 22:47:58 -0700

    clothingItem schema

--- models/clothingItem.js
+++ models/clothingItem.js
@@ -1,5 +1,27 @@
 const mongoose = require("mongoose");
 
-const clothingItemsSchema = new mongoose.Schema({});
+const clothingItemsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  weather: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  imageURL: {
+    type: URL,
+    required: true,
+    // MAKE SURE IT MATCHES EITHER HOT COLD OR WARM
+    validator(value) {
+      return validator.isURL(value);
+    },
+    message: "You must enter a valid URL",
+  },
+});
 
 module.exports = mongoose.model("item", clothingItemsSchema);

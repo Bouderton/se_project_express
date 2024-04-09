16854c64ff3f6abdd161b294b2f9148235d4726d
bouderton <rmkhazal@gmail.com>
2024-04-09 11:56:51 -0700

    likeItem dislikeItem router

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -10,5 +10,7 @@ const {
 router.get("/", getItems);
 router.post("/", createItem);
 router.delete("/:itemId", deleteItem);
+router.put("/:itemId/likes", likeItem);
+router.delete("/:itemId/likes", dislikeItem);
 
 module.exports = router;

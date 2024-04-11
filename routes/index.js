8868902f645e49895f28ef4ed451d0caf661d2f0
bouderton <rmkhazal@gmail.com>
2024-04-10 23:15:08 -0700

    move invalid route catcher

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,12 @@ const router = require("express").Router();
 const userRouter = require("./users");
 const itemsRouter = require("./clothingItems");
 
-router.use((req, res) => {
-    return res.status(404).send("Route not found");
-})
 
 router.use("/users", userRouter);
 router.use("/items", itemsRouter);
+
+router.use((req, res) => {
+    return res.status(404).send("Route not found");
+});
 
 module.exports = router;

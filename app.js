56b5f373521ffe9bcd6d32bb569b1d5fa71f92bd
bouderton <rmkhazal@gmail.com>
2024-04-27 22:03:58 -0700

    installed cors package

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const mainRouter = require("./routes/index");
+const cors = require("cors");
 
 const app = express();
 const { PORT = 3001 } = process.env;
@@ -16,6 +17,7 @@ app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
 
+// Old hardcoded user
 // app.use((req, res, next) => {
 //   req.user = {
 //     _id: '6610c4f20ecc10975709f402',
@@ -25,3 +27,4 @@ app.listen(PORT, () => {
 
 app.use(express.json());
 app.use("/", mainRouter);
+app.use(cors());

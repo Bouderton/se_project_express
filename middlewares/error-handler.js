80d32f343e81122de123b6ba4463bd7ca5254d42
bouderton <rmkhazal@gmail.com>
2024-06-16 10:37:33 -0700

    error-handler removed

--- middlewares/error-handler.js
+++
@@ -1,9 +0,0 @@
-const errorHandler = (req, res, err, next) => {
-  console.error(err);
-  const { statusCode = 500, message } = err;
-  return res
-    .status(statusCode)
-    .send({ message: statusCode ? "Internal Server Error" : message });
-};
-
-module.exports = errorHandler;

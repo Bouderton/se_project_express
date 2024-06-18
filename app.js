6f30db424e887a0a727c180841592653e2ef5426
bouderton <rmkhazal@gmail.com>
2024-06-17 21:30:38 -0700

    custom winstone loggers

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express();
 const { PORT = 3001 } = process.env;
 const errorHandler = require("./middlewares/errorhandler");
 const { errors } = require("celebrate");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
@@ -22,6 +23,8 @@ app.listen(PORT, () => {
 
 app.use(cors());
 app.use(express.json());
+app.use(requestLogger);
 app.use("/", mainRouter);
+app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
---
+++ middlewares/logger.js
@@ -0,0 +1,31 @@
+const winston = require("winston");
+const expressWinston = require("express-winston");
+
+module.exports.messageFormat = winston.format.combine(
+  winston.format.timestamp(),
+  winston.format.printf(
+    ({ level, message, meta, timestamp }) =>
+      `${timestamp} ${level}: ${meta.error?.stack || message}`,
+  ),
+);
+
+module.exports.requestLogger = expressWintson.logger({
+  transports: [
+    new winston.transports.Console({
+      format: messageFormat,
+    }),
+    new winston.transports.File({
+      filename: "request.log",
+      format: winston.format.json(),
+    }),
+  ],
+});
+
+module.exports.errorLogger = expressWinston.errorLogger({
+  transports: [
+    new winston.transports.File({
+      filename: "error.log",
+      format: winston.format.json(),
+    }),
+  ],
+});
